fc6fa1b21f8f4a900e8e56887d2566cc730728f7
rahul-gabbar <69715476+rahul786-gabbar@users.noreply.github.com>
2021-10-02 10:18:34 +0530

    Update GUI.py
    Commited

--- GUI.py
+++ GUI.py
@@ -59,7 +59,7 @@ top.configure(background='black')
 
 label=Label(top,background='green', font=('Calibri',16,'italic'))
 sign_image = Label(top)
-
+# tkinter classes
 def classify(file_path):
     global label_packed
     image = Image.open(file_path)
