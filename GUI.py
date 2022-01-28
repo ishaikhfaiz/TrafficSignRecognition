2420762d9079fb254df42a9c2917491bbece04cb
rahul-gabbar <69715476+rahul-gabbar@users.noreply.github.com>
2022-01-28 19:59:22 +0530

    Update GUI.py
    news api

--- GUI.py
+++ GUI.py
@@ -102,15 +102,7 @@ def takeCommand():
         return "None"
     return query
 # news api
-def news():
-    newsapi = NewsApiClient(api_key='5840b303fbf949c9985f0e1016fc1155')
-    speak("What topic you need the news about")
-    topic = takeCommand()
-    data = newsapi.get_top_headlines(
-        q=topic, language="en", page_size=5)
-    newsData = data["articles"]
-    for y in newsData:
-        speak(y["description"])
+
 # command processing
 def car_assistant():
     while True:
