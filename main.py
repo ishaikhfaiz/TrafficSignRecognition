846d76278f8f393df37ab32f5284b5ebf80a1447
rahul-gabbar <69715476+rahul-gabbar@users.noreply.github.com>
2022-03-13 15:28:26 +0530

    Update main.py
    test code

--- main.py
+++ main.py
@@ -127,7 +127,49 @@ for i in range(NUM_CATEGORIES):
         except:
             print("Error in " + img)
 
+# Changing the list to numpy array
+image_data = np.array(image_data)
+image_labels = np.array(image_labels)
 
+shuffle_indexes = np.arange(image_data.shape[0])
+np.random.shuffle(shuffle_indexes)
+image_data = image_data[shuffle_indexes]
+image_labels = image_labels[shuffle_indexes]
+
+X_train, X_val, y_train, y_val = train_test_split(image_data, image_labels, test_size=0.3, random_state=42, shuffle=True)
+
+X_train = X_train/255
+X_val = X_val/255
+
+print("X_train.shape", X_train.shape)
+print("X_valid.shape", X_val.shape)
+print("y_train.shape", y_train.shape)
+print("y_valid.shape", y_val.shape)
+
+model = keras.models.Sequential([
+
+    keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
+
+                        input_shape=(IMG_HEIGHT, IMG_WIDTH, channels)),
+
+    keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(rate=0.5),
+
+    keras.layers.Dense(43, activation='softmax')
+])
 
 lr = 0.001
 epochs = 30
